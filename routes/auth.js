377dabab2dce3b15c814b99bd7258b39cde560e9
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-06-16 11:48:29 +0600

    admin setup

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,17 @@
 const express = require('express')
 
 const router = express.Router();
-//import 
+
+// middlewares
+
+const {authCheck} = require('../middlewares/auth')
+
+// controller
 const {createOrUpdateUser} = require('../controllers/auth')
 
-router.get('/create-or-update-user', createOrUpdateUser)
+
+
+router.post('/create-or-update-user', authCheck, createOrUpdateUser)
+
 
 module.exports = router
