7769cdf598809c3594914bd533436ccc9ae802f2
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-07-25 00:32:52 +0600

    cloudinary router and controller create

---
+++ routes/cloudinary.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const router = express.Router();
+
+// middleWares
+
+const {authCheck,adminCheck} = require('../middlewares/auth')
+
+// controller
+const {upload,remove} = require('../controllers/cloudinary');
+
+
+
+//routes
+router.post('/uploadimages',authCheck,adminCheck,upload);
+router.post('/removeimages',authCheck,adminCheck,remove);
+
+module.exports = router;
