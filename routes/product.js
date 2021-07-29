1a25ea278856ed59c2b473a2da2886e30e047ae4
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-07-29 15:58:58 +0600

    get products total count

--- routes/product.js
+++ routes/product.js
@@ -7,17 +7,19 @@ const router = express.Router();
 const {authCheck,adminCheck} = require('../middlewares/auth')
 
 // controller
-const {create,listAll,remove,read,update,list} = require('../controllers/product');
+const {create,listAll,remove,read,update,list,productsCount} = require('../controllers/product');
 
 
 
 //routes
 router.post('/product',authCheck,adminCheck,create);
 router.get('/products/:count',listAll); //products/100
+router.get('/products/total', productsCount)
 router.delete('/product/:slug', authCheck,adminCheck,remove);
 router.get('/product/:slug', read);
 router.put('/product/:slug',authCheck,adminCheck,update)
 
 router.post('/products', list)
+
 
 module.exports = router
