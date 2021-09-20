2d039471c5860cd3cafd3d015bb1f216e629f396
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-09-20 12:04:30 +0600

    filter by sub category

--- controllers/product.js
+++ controllers/product.js
@@ -231,10 +231,19 @@ const  handleStars = (req,res,stars) =>{
          res.json(products)
       });
 
-   })
+   });
+
+};
+const handleSub = async(req,res,sub) =>{
+   const products = await Product.find({subs:sub})
+   .populate('category','_id name')
+   .populate('subs','_id name')
+   .populate('postedBy', '_id name')
+   .exec();
+   res.json(products);
 }
 exports.searchFilters = async(req,res) =>{
-          const {query,price,category,stars} = req.body;
+          const {query,price,category,stars,sub} = req.body;
 
           if(query){
              console.log('query',query)
@@ -254,5 +263,10 @@ exports.searchFilters = async(req,res) =>{
           if(stars){
             console.log('stars-->', stars)
             await handleStars(req,res,stars)
+          }
+
+          if(sub){
+            console.log('sub-->', sub)
+            await handleSub(req,res,sub)
           }
 }
