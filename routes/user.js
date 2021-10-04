cbed140207fb48ce215df16035c8dca335d64c22
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2021-10-04 13:29:43 +0600

    save order to database

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,20 @@
 const express = require('express')
 
-const router = express.Router();
+const router = express.Router()
+// middlewares
+const {authCheck} = require('../middlewares/auth')
 
-router.get('/user', (req,res) =>{
-    res.json({
-       data:'hey you hate user api'
-    })
-})
+// controllers
+
+const {userCart} = require("../controllers/user")
+
+router.post('/user/cart', authCheck,userCart); // save Cart
+
+// router.get('/user', (req,res) =>{
+//     res.json({
+//        data:'hey you hate user api'
+//     })
+// });
+
 
 module.exports = router
