bbed11e4025ac20c774f582487f58edb5f6241f1
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2022-01-21 00:53:56 +0600

    charging Actual Cart Total

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -7,9 +7,14 @@ const stripe = require('stripe')(process.env.Stripe_Secret)
 exports.createPaymentIntent = async(req,res) =>{
     // later apply coupon 
     // later calculate price 
-
+    // 1 find user 
+      const user = await User.findOne({email:req.user.email}).exec()
+      // get user cart total
+      const {cartTotal} = await Cart.findOne({orderedBy: user._id}).exec()
+      console.log('Cart total', cartTotal)
+      // create paymentIntent with order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
-        amount:100,
+        amount:cartTotal * 100,
         currency:'usd',
     });
 
