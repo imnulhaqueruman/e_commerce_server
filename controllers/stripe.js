d64c7162e9cb89297ddf50a0934daa22ef250d4d
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2022-01-21 01:28:03 +0600

    charge with coupon

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -5,20 +5,32 @@ const Coupon = require ('../models/coupon')
 const stripe = require('stripe')(process.env.Stripe_Secret)
 
 exports.createPaymentIntent = async(req,res) =>{
+    //console.log(req.body)
+    const {couponApplied} = req.body
     // later apply coupon 
     // later calculate price 
     // 1 find user 
       const user = await User.findOne({email:req.user.email}).exec()
       // get user cart total
-      const {cartTotal} = await Cart.findOne({orderedBy: user._id}).exec()
-      console.log('Cart total', cartTotal)
+      const {cartTotal,totalAfterDiscount} = await Cart.findOne({orderedBy: user._id}).exec()
+      console.log('Cart total', cartTotal,'after discount',totalAfterDiscount)
+
+      let finalAmount = 0
+      if(couponApplied && totalAfterDiscount){
+          finalAmount = (totalAfterDiscount * 100)
+      }else{
+          finalAmount = (cartTotal * 100)
+      }
       // create paymentIntent with order amount and currency
     const paymentIntent = await stripe.paymentIntents.create({
-        amount:cartTotal * 100,
+        amount:finalAmount,
         currency:'usd',
     });
 
     res.send({
         clientSecret:paymentIntent.client_secret,
+        cartTotal,
+        totalAfterDiscount,
+        payable:finalAmount,
     })
 }
--- controllers/user.js
+++ controllers/user.js
@@ -90,11 +90,12 @@ exports.applyCouponToUserCart = async(req,res) =>{
      console.log('cartTotal', cartTotal, 'discount', validCoupon.discount);
      // calculate the total after discount
      let totalAfterDiscount = (cartTotal - (cartTotal * validCoupon.discount)/100).toFixed(2);
+     console.log("---------->",totalAfterDiscount)
      Cart.findOneAndUpdate(
          {orderedBy: user._id},
          {totalAfterDiscount},
          {new:true}
-        );
+        ).exec()
     res.json(totalAfterDiscount)
 
 }
