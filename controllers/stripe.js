dfb7f99dbcf42a36a374b7b92f87714fbba884ee
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2022-01-22 01:04:31 +0600

    bug fixed

--- controllers/stripe.js
+++ controllers/stripe.js
@@ -5,32 +5,36 @@ const Coupon = require ('../models/coupon')
 const stripe = require('stripe')(process.env.Stripe_Secret)
 
 exports.createPaymentIntent = async(req,res) =>{
-    //console.log(req.body)
-    const {couponApplied} = req.body
-    // later apply coupon 
-    // later calculate price 
-    // 1 find user 
-      const user = await User.findOne({email:req.user.email}).exec()
-      // get user cart total
-      const {cartTotal,totalAfterDiscount} = await Cart.findOne({orderedBy: user._id}).exec()
-      console.log('Cart total', cartTotal,'after discount',totalAfterDiscount)
-
-      let finalAmount = 0
-      if(couponApplied && totalAfterDiscount){
-          finalAmount = (totalAfterDiscount * 100)
-      }else{
-          finalAmount = (cartTotal * 100)
-      }
-      // create paymentIntent with order amount and currency
-    const paymentIntent = await stripe.paymentIntents.create({
-        amount:finalAmount,
-        currency:'usd',
-    });
-
-    res.send({
-        clientSecret:paymentIntent.client_secret,
-        cartTotal,
-        totalAfterDiscount,
-        payable:finalAmount,
-    })
+    try{
+        const {couponApplied} = req.body
+        // later apply coupon 
+        // later calculate price 
+        // 1 find user 
+          const user = await User.findOne({email:req.user.email}).exec()
+          // get user cart total
+          const {cartTotal,totalAfterDiscount} = await Cart.findOne({orderedBy: user._id}).exec()
+          console.log('Cart total', cartTotal,'after discount',totalAfterDiscount)
+    
+          let finalAmount = 0
+          if(couponApplied && totalAfterDiscount){
+              finalAmount = parseInt(totalAfterDiscount *100)
+          }else{
+              finalAmount = (cartTotal * 100)
+          }
+          // create paymentIntent with order amount and currency
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount:finalAmount,
+            currency:'usd',
+        });
+    
+        res.send({
+            clientSecret:paymentIntent.client_secret,
+            cartTotal,
+            totalAfterDiscount,
+            payable:finalAmount,
+        })
+    } catch(err){
+      console.log(err)
+    }
+    
 }
