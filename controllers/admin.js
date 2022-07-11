954097efa2c1ffac6bdaf0705aa9b1c7f8329fc3
imnulhaqueruman <imnulhaquerumantalukder1999@gmail.com>
2022-07-11 14:16:33 +0600

    admin controllers setup

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,10 +2,10 @@ const Order = require('../models/order')
 
 exports.orders = async(req,res) =>{
     let AllOrders  = await Order.find({})
-    .sort('-createdAt')
+    .sort('createdAt')
     .populate('products.product')
     .exec()
-  res.json(AllOrders)
+    res.send(AllOrders)
 };
 
 exports.orderStatus = async(req,res) =>{
--- routes/admin.js
+++ routes/admin.js
@@ -8,6 +8,6 @@ const {orders, orderStatus} = require('../controllers/admin')
 
 // routes 
 router.get('/admin/orders', authCheck,adminCheck,orders)
-router.get('/admin/order-status', authCheck,adminCheck,orderStatus)
+router.put('/admin/order-status', authCheck,adminCheck,orderStatus)
 
 module.exports = router
