4a9e49c2d9a00ca11861db5976e84da9dac5cb72
Imnul Haque Ruman <imnulhaquerumantalukder1999@gmail.com>
2026-09-24 19:35:08 +0600

    Update product.js

--- controllers/product.js
+++ controllers/product.js
@@ -28,13 +28,12 @@ exports.create = async(req,res) =>{
       console.log(req.body);
       req.body.slug = slugify(req.body.title);
       const newProduct = await new Product(req.body).save()
-      res.json(newProduct);
+      res.json(newPro
    }catch(err){
     console.log(err)
     //res.status(400).send('Create Product failed')  ;
     res.status(400).json({
        err:err.message,
-    });
    }
 };
 
